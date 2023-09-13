56c2ab99a2818576959a352f6f7fe05d3419f688
Yufan Lu <yufan.lu@snapchat.com>
2023-09-13 11:27:47 -0700

    build

--- webapp/build_image.sh
+++ webapp/build_image.sh
@@ -5,12 +5,15 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-gsutil cp -r "gs://vf-unity-data/$1" "./static/$1"
+gsutil cp -r "gs://vf-unity-data/$1" "./static"
 
 template=`cat ./templates/game.html.template`
 replaced="${template//\{vf-0912v0\}/$1}"
 echo "$replaced" > ./templates/game.html
 
 gcloud builds --project ysong-chat submit --tag gcr.io/ysong-chat/webapp0912:$(git rev-parse --short HEAD)-$(openssl rand -hex 4) .
+
+rm -r "./static/$1"
+rm ./templates/game.html
 
 echo "Done, don't forget to commit the change"
