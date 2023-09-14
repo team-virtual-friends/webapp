ecab40d23927bfb1155dcb0d2a4150fdf8a0cbb3
Yufan Lu <yufan.lu@snapchat.com>
2023-09-14 00:03:10 -0700

    fix buffer bug

--- virtualfriends/web_socket/ws_api.py
+++ virtualfriends/web_socket/ws_api.py
@@ -112,7 +112,7 @@ def stream_reply_speech_handler(request:ws_message_pb2.StreamReplyMessageRequest
     message_dicts = [json.loads(m) for m in request.json_messages]
     message_dicts.append({"role": "user", "content": text})
     
-    reply_message_iter = llm_reply.stream_infer_reply(message_dicts, request.mirrored_content.character_name, request.custom_prompts)
+    reply_message_iter = llm_reply.stream_infer_reply(message_dicts, request.mirrored_content.character_name.lower(), request.custom_prompts)
 
     def send_reply(reply_text:str, index:int, is_stop:bool):
         response = ws_message_pb2.StreamReplyMessageResponse()
@@ -164,7 +164,7 @@ def stream_reply_speech_handler(request:ws_message_pb2.StreamReplyMessageRequest
             continue
 
         # logger.info("current: " + current)
-        splited = re.split("\.|;|\!|\?|:|,", current)
+        splited = re.split("\.|;|\!|\?|:|,|。|；|！|？|：|，", current)
         if len(splited) == 0:
             pass
         elif len(splited) == 1:
@@ -176,4 +176,6 @@ def stream_reply_speech_handler(request:ws_message_pb2.StreamReplyMessageRequest
             send_reply(buffer, index, False)
             buffer = splited[-1]
             index += 1
-    send_reply("", index, True)
+    if len(buffer.strip()) > 0:
+        send_reply(buffer, index, True)
+    send_reply("", index + 1, True)
