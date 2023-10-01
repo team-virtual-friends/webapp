7d5bda745f58c7897bf21038289a8b041e9fcc79
sieglu2 <135094984+sieglu2@users.noreply.github.com>
2023-09-30 23:50:15 -0700

    signup (#55)

--- data_access/create_table.py
+++ data_access/create_table.py
@@ -3,11 +3,26 @@ import os
 import hashlib
 
 # Initialize client
-client = datastore.Client.from_service_account_json('../webapp/ysong-chat-845e43a6c55b.json')
+# client = datastore.Client.from_service_account_json('../webapp/ysong-chat-845e43a6c55b.json')
 
 
-def create_and_insert_user(user_id, pwd):
-    # Create a new user entity in the "users_db" namespace
+def create_and_insert_user(client, user_id, username, pwd):
+    # Check if a user with the same username or user_id already exists
+    query1 = client.query(kind='User', namespace='users_db')
+    query1.add_filter('username', '=', username)
+
+    query2 = client.query(kind='User', namespace='users_db')
+    query2.add_filter('user_id', '=', user_id)
+
+    results_query1 = list(query1.fetch(limit=1))
+    results_query2 = list(query2.fetch(limit=1))
+
+    # Check if either of the queries found a match
+    if results_query1 or results_query2:
+        # A user with the same username or user_id already exists
+        return False
+
+    # If no matching user exists, proceed to create and insert the new user entity
     key = client.key('User', namespace='users_db')
     user_entity = datastore.Entity(key=key)
 
@@ -17,6 +32,7 @@ def create_and_insert_user(user_id, pwd):
 
     user_entity.update({
         'user_id': user_id,
+        'username': username,
         'hashed_pwd': hashed_password.hex(),
         'salt': salt.hex(),
     })
@@ -24,9 +40,10 @@ def create_and_insert_user(user_id, pwd):
     # Save the user entity
     client.put(user_entity)
     print(f"New user added with ID: {user_entity.key.id}")
+    return True  # Successfully created and inserted the user entity
 
 
-def create_and_insert_character():
+def create_and_insert_character(client):
     # Create a new character entity in the "characters_db" namespace
     key = client.key('Character', namespace='characters_db')
     character_entity = datastore.Entity(key=key)
@@ -43,6 +60,5 @@ def create_and_insert_character():
 
 
 # Execute the functions
-create_and_insert_user("test@gmail.com", "123456")
+# create_and_insert_user("test@gmail.com", "test", "123456")
 # create_and_insert_character()
-
