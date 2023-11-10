fd2b7a07a5509119a6c788f72bb144fefc78dc92
Yufan Lu <myhappyland3113@gmail.com>
2023-11-10 11:50:13 -0800

    fix client

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ specialCharacters = {
 
 unity_gcs_bucket = "vf-unity-data"
 unity_gcs_folders = [
-    "20231110005947-456be4d-8c9808f5",
+    "20231110114117-3b82ad4-181c640a",
     "20231105171240-3cccaa0-04e46593",
 ]
 unity_index_html_replacements = {
